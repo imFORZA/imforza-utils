6099f34ead9beb1ceb0e4858491befd57a67e381
Santiago Garza <santiago.garza@imforza.com>
2017-06-21 15:42:24 -0700

    Added static method for scheduling cron jobs

--- class-imforza-utils.php
+++ class-imforza-utils.php
@@ -1,6 +1,7 @@
 <?php
 
 if( ! class_exists( 'IMFORZA_Utils' ) ) {
+
 /**
  * Group of utility methods for use by imFORZA
  * All methods are static, this is just a sort of namespacing class wrapper.
@@ -20,13 +21,31 @@ class IMFORZA_Utils {
 		elseif ( site_url() && false === strpos( site_url(), '.' ) ) {
 			$development_mode = true;
 		}
-    
+
     /**
 		 * Filter the imforza development mode.
 		 *
 		 * @param bool $development_mode Is imforza development mode active.
 		 */
 		return apply_filters( 'imforza_development_mode', $development_mode );
-	}  
+	}
+
+	/**
+	 * Schedule cron Job.
+	 *
+	 * @static
+	 */
+	public static function schedule_cron( $hook, $recurrence = null, $time = null ) {
+		// If reccurence and time not sent, then set defaults.
+		$recurrence = $recurrence ?? 'daily';
+		$time = $time ?? time();
+
+		// Schedule.
+		if ( ! wp_next_scheduled( $hook ) ) {
+			wp_schedule_event( $time, $recurrence , $hook );
+		}
+	}
+
 } // end class.
+
 } // end if.
